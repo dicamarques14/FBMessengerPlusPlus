cb3bab499b312cd28802d59fbfc997d1460e0a15
Dg <dicamarques14@users.noreply.github.com>
2017-06-18 13:23:32 +0100

    Create fbmessengerpp.meta.js

---
+++ fbmessengerpp.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name Hide Messenger Sidebar
+// @namespace https://github.com/dicamarques14/
+// @version 0.1
+// @description hide messengers list of conversations
+// @match https://www.messenger.com/*
+// @copyright 2017+, TheDg
+// @author       Dg
+// @require http://code.jquery.com/jquery-latest.js
+// @grant        none
+// @run-at document-idle
+// @updateURL    https://github.com/dicamarques14/FBMessengerPlusPlus/raw/master/fbmessengerpp.meta.js
+// @downloadURL  https://github.com/dicamarques14/FBMessengerPlusPlus/raw/master/fbmessengerpp.user.js
+// @updateVersion 0.1
+// ==/UserScript==
