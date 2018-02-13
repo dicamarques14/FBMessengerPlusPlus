e51b57aec8862a7c58598411a32f51e23a36eae3
Dg <dicamarques14@users.noreply.github.com>
2018-02-13 09:08:57 +0000

    Update fbmessengerpp.meta.js

--- fbmessengerpp.meta.js
+++ fbmessengerpp.meta.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name FB Messenger ++
 // @namespace https://github.com/dicamarques14/
-// @version 0.2
+// @version 0.3
 // @description hide messengers list of conversations
 // @match https://www.messenger.com/*
 // @match https://www.facebook.com/messages/*
-// @copyright 2017+, TheDg
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @author       Dg
 // @require http://code.jquery.com/jquery-latest.js
 // @grant        none
 // @run-at document-idle
 // @updateURL    https://github.com/dicamarques14/FBMessengerPlusPlus/raw/master/fbmessengerpp.meta.js
 // @downloadURL  https://github.com/dicamarques14/FBMessengerPlusPlus/raw/master/fbmessengerpp.user.js
-// @updateVersion 0.2
 // ==/UserScript==
